b0b3738ed42db5cf6d971797922d37c940f8c73c
Mauro Busso <mauro.busso12@gmail.com>
2023-06-24 21:13:56 +0100

    small changes to img

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import rcopLogo from "../assets/RCOP-logo.png"
+import ada1 from "../assets/ada1.jpg"
 
 export default function Main() {
     return (
@@ -8,9 +9,9 @@ export default function Main() {
 
           <h2 className="flex justify-center py-4">About Us</h2>
 
-          <div className="px-12 md:flex item-center">
-            <div className="relative mx-auto w-60 h-60 my-20 rounded-xl lg:w-80 lg:h-80 border-2 border-black">
-                <img alt="img" layout="fill" className="object-cover w-full h-auto rounded-xl"></img>
+          <div className="pb-24 px-12 md:flex md:items-center">
+            <div className="relative mx-auto w-60 h-60 my-20 rounded-xl lg:w-80 lg:h-80 border-2">
+                <img src={ada1} alt="img" layout="fill" className="object-cover w-full h-auto rounded-xl"></img>
             </div>
             <div className="text-lg md:px-8 lg:px-16 md:mt-20 md:max-w-2xl">
               <h4 className="flex justify-center">Compassionate healthcare</h4>
