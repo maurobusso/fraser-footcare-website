4581f5e853d05407db67446c3de205ed5f5d48b1
Mauro Busso <mauro.busso12@gmail.com>
2023-06-28 12:31:57 +0100

    small colour changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import React from "react"
 export default function Footer() {
     return (
       <>
-        <footer className="footer">
+        <footer className="footer mt-40">
           <div className="">
             <div>
               <p className="flex justify-center p-4">Please send us a message, or call us for an appointment (555) 555-5555.</p>
             </div>
             <div>
               <h5 className="flex justify-center py-4">Hours</h5>
-              <div className="flex justify-center py-4">
+              <div id="contact" className="flex justify-center py-4">
                 <ol >
                   <li>Mon: 9 am - 5 pm</li>
                   <li>Tue: 9 am - 5 pm</li>
