3bf653966acb64b8f998c50e6a858ef6cbd92e3f
Mauro Busso <mauro.busso12@gmail.com>
2023-07-05 17:17:02 +0100

    latest changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,23 +8,23 @@ export default function Navbar() {
         <nav className="h-80 sm:bg-cover bg-no-repeat bg-contain lg:bg-center lg:bg-auto" style={{backgroundImage: `url(${adaLogo})`}}>
           <div className="flex">
             <div className="mx-4 flex justify-start">
-              <ul className="flex gap-2 items-center justify-center md:justify-start">
+              {/* <ul className="flex gap-2 items-center justify-center md:justify-start">
                 <li className="my-2 md:mx-4">
-                  <button>0</button>
+                  <button></button>
                 </li>
                 <li className="my-2 md:mx-4">
-                  <button>0</button>
+                  <button></button>
                 </li>
-              </ul>
+              </ul> */}
             </div>
             <div className="flex justify-end">
               <ul className="flex gap-4 items-center justify-center mx-4 md:justify-end">
                 <li className="my-2 md:mx-8">
                   <Link to="contact" smooth={true} duration={500}>Contact</Link>
                 </li>
-                <li className="my-2 md:mx-8">
+                {/* <li className="my-2 md:mx-8">
                   <Link to="contact" smooth={true} duration={500}>About Us</Link>
-                </li>
+                </li> */}
                 <li className="my-2 md:mx-8">
                   <Link to="services" smooth={true} duration={500}>Our Services</Link>
                 </li>
